70792ced90ea3797d764f8d5a3ce24fff0360e7b
anik12136 <anikbiswas431@gmail.com>
2025-02-17 02:11:25 +0600

    fix navigation and use loading

--- src/routes/ProtectedRoutes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes/ProtectedRoutes.jsx
@@ -1,20 +1,27 @@
 import { useContext } from "react";
 import { AuthContext } from "../../providers/AuthProviders";
 import { Navigate, useLocation } from "react-router-dom";
+import { Commet } from "react-loading-indicators";
 
+const ProtectedRoutes = ({ children }) => {
+  const location = useLocation();
+  const { user, loading } = useContext(AuthContext);
 
-const ProtectedRoutes = ({children}) => {
-    const location = useLocation();
-    // context value
-    const {user} = useContext(AuthContext);
-    
-    // If the user does not exist then go to the login route, else to the visiting route
-    if(user){
-        return children;
-    }
+  // Wait until loading finishes before deciding to navigate
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Commet color="#cc7731" size="large" text="" textColor="#NaNNaNNaN" />
+      </div>
+    );
+  }
 
-   
-    return <Navigate to={"/login"} state={location.pathname}></Navigate>
+  // Show children if authenticated, otherwise navigate to login
+  if (user) {
+    return children;
+  }
+
+  return <Navigate to="/login" state={{ from: location }} replace />;
 };
 
 export default ProtectedRoutes;
